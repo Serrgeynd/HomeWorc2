6e65b4c019aac817d5cf0c257a1a4f055a0495c5
sdudin <serrgeynd@rambler.ru>
2020-09-06 23:20:09 +0300

    Домашнее задание, возникли проблемы с задачей 8 пришлось разбить на 2 части, если объединять в одну задачуто он ругается на переменную I

---
+++ src/HomeWork2/Zadacha3.java
@@ -0,0 +1,14 @@
+package HomeWork2;
+
+import java.util.Scanner;
+
+public class Zadacha3 {
+    public static void main (String[] args){
+        System.out.println("Введите целое цисло:");
+        Scanner scanner = new Scanner(System.in);
+        int x;
+        x = scanner.nextInt();
+        if (x>(0)) System.out.println(x+1);
+        else System.out.println(x);
+    }
+}
