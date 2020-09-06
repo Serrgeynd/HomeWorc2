f5c265596dd00f162a33343cf115e9cde4ce5aff
sdudin <serrgeynd@rambler.ru>
2020-09-06 23:18:28 +0300

    Домашнее задание, возникли проблемы с задачей 8 пришлось разбить на 2 части, если объединять в одну задачуто он ругается на переменную I

---
+++ src/HomeWork2/Zadacha8.java
@@ -0,0 +1,43 @@
+package HomeWork2;
+
+import java.util.Arrays;
+
+import static sun.swing.MenuItemLayoutHelper.max;
+
+public class Zadacha8 {
+    public static void main(String[] args) {
+        int[] mask = {1, -10, 5, 6, 45, -34, 0, 32, 56, -1, 2, -2};
+        int max = max(mask);
+        System.out.println("Максимальное значение:" + max);
+
+        int summch = 0;
+        for (int i = 0; i < mask.length; i++) if ((mask[i] < 0) & (mask[i] % 2 == 0)) summch += mask[i];
+        System.out.println("Cумма отрицательных четных элементов:" + summch);
+
+        int sump = 0;
+        for (int i = 0; i < mask.length; i++) if (mask[i] > 0) sump += mask[i];
+        System.out.println("Cумма положительных элементов:" + sump);
+
+        int plus = 0;
+
+
+        for (int i = 0; i < mask.length; i++)
+            if (mask[i] > 0) {
+                plus++;
+
+
+                System.out.println("Количество положительных элементов:" + plus);
+
+
+                }
+            }
+}
+
+
+
+
+
+
+
+
+
