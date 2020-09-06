5bc5fa6f201521c961dd7f2754081ed9a459342c
sdudin <serrgeynd@rambler.ru>
2020-09-06 23:19:06 +0300

    Домашнее задание, возникли проблемы с задачей 8 пришлось разбить на 2 части, если объединять в одну задачуто он ругается на переменную I

---
+++ src/HomeWork2/Zadacha6.java
@@ -0,0 +1,24 @@
+package HomeWork2;
+
+import java.util.Scanner;
+
+public class Zadacha6 {
+    public static void main(String[] args){
+        System.out.println("Введите целое число:");
+        Scanner scanner = new Scanner(System.in);
+        int x;
+        x = scanner.nextInt();
+        int y;
+        y = x%2;
+        int z;
+        if (x==(0)) {System.out.println("Нулевое число");}
+        else if ((x>0) & (y==0)) {System.out.println("Положительное четное число");}
+        else if ((x>0)& (y!=0)) {System.out.println("Положительное нечетное число");}
+        else if ((x<0) & (y!=0)) {System.out.println("Отрицательное нечетное число");}
+        else if ((x<0) & (y==0)) {System.out.println("Отрицательное четное число");}
+
+
+
+
+    }
+}
