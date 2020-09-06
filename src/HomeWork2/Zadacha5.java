4e4d9af338a52111841b5140d6e3db26dd3dc1e1
sdudin <serrgeynd@rambler.ru>
2020-09-06 23:19:30 +0300

    Домашнее задание, возникли проблемы с задачей 8 пришлось разбить на 2 части, если объединять в одну задачуто он ругается на переменную I

---
+++ src/HomeWork2/Zadacha5.java
@@ -0,0 +1,19 @@
+package HomeWork2;
+
+import java.util.Scanner;
+
+public class Zadacha5 {
+    public static void main (String[] args) {
+        System.out.println("Введите 3 целых числа:");
+        Scanner scanner = new Scanner(System.in);
+        int x;
+        int y;
+        int z;
+        x = scanner.nextInt();
+        y = scanner.nextInt();
+        z = scanner.nextInt();
+        if ((x<=y)&(x<=z)) System.out.println(x);
+        if ((y<=x)&(y<=z)) System.out.println(y);
+        if ((z<=y)&(z<=x)) System.out.println(z);
+            }
+}
