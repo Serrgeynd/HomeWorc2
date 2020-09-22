b40701e8bd58be9bd5c4257bcf9ca9412411c0f6
sdudin <serrgeynd@rambler.ru>
2020-09-22 22:41:51 +0300

    Изменил логику

--- src/Task8v2.java
+++ src/Task8v2.java
@@ -4,28 +4,27 @@ public class Task8v2 {
     public static void main(String[] args) {
         int[] mask = {1, -10, 5, 6, 45, -34, 0, 32, 56, -1, 2, -2};
         int max = max(mask);
-        int sumplus = 0;
-        for (int i = 0; i < mask.length; i++) if (mask[i] > 0) sumplus += mask[i];
-        int summinus = 0;
-        for (int i = 0; i < mask.length; i++) if ((mask[i] < 0) & (mask[i] % 2 == 0)) summinus += mask[i];
+        int pluselementSum = 0;
+        for (int i = 0; i < mask.length; i++) if (mask[i] > 0) pluselementSum += mask[i];
+        int minuselementSum = 0;
+        for (int i = 0; i < mask.length; i++) if ((mask[i] < 0) & (mask[i] % 2 == 0)) minuselementSum += mask[i];
         int plus = 0;
         for (int i = 0; i < mask.length; i++)
             if (mask[i] > 0) {
                 plus++;
             }
         int minus = 0;
-        int middleminus = 0;
+        int minuselementAverage = 0;
         for (int i = 0; i < mask.length; i++)
             if (mask[i] < 0) {
                 minus++;
-                if (mask[i] < 0) middleminus += mask[i];
+                if (mask[i] < 0) minuselementAverage += mask[i];
             }
         System.out.println("Максимальное значение:" + max);
-
-        System.out.println("Cумма положительных элементов:" + sumplus);
-        System.out.println("Cумма отрицательных четных элементов:" + summinus);
+        System.out.println("Cумма положительных элементов:" + pluselementSum);
+        System.out.println("Cумма отрицательных четных элементов:" + minuselementSum);
         System.out.println("Количество  положительных элементов:" + plus);
-        System.out.println("Среднее арифметическое отрицательных чисел:" + middleminus / minus);
+        System.out.println("Среднее арифметическое отрицательных чисел:" + minuselementAverage / minus);
     }
 
 }
